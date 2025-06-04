c0e542048bdf525cc9fb0bb47189e375ef888326
frossi <faaustor@gmail.com>
2025-06-04 20:00:10 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
