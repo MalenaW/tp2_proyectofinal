4d5aa1a3b7631a76a9f4cc724ecfe612170953fa
Luciano Amato <luciano2697@outlook.com.ar>
2025-06-04 22:48:30 -0300

    Added: README | require => import

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express');
-require('dotenv').config();
+import express from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+//Ejemplo de un endpoint básico
+app.use("/peliculas", (req, res) => {
+  res.send("Endpoint de películas no implementado aún.");
+})
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
